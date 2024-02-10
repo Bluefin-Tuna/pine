fce4adf98f84fcbe92a63b5355a4998e0f22ca2e
DumbassNamedTuna <50528857+Bluefin-Tuna@users.noreply.github.com>
2024-02-10 14:31:40 -0500

    feat: added app wrapper div

--- frontend.pine/src/App.tsx
+++ frontend.pine/src/App.tsx
@@ -1,11 +1,15 @@
 import './App.css'
 import Input from "./components/Input.tsx";
+import Graph from "./components/Graph.tsx";
 
 function App() {
   //const [count, setCount] = useState(0)
 
   return (
-    <Input />
+    <div className="wrapper">
+      <Input />
+      {/* <Graph /> */}
+    </div>
   )
 }
 
